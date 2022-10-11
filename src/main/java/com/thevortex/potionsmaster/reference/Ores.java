17bdc982bc9c279a5876e895439332807cd75a92
thevortex <james.g.bowley@gmail.com>
2022-10-10 18:27:02 -0600

    Update Ores.java
    tag changes

--- src/main/java/com/thevortex/potionsmaster/reference/Ores.java
+++ src/main/java/com/thevortex/potionsmaster/reference/Ores.java
@@ -8,21 +8,21 @@ import net.minecraft.world.level.block.Block;
 
 public class Ores {
 
-    public static final TagKey<Block> DIAMOND = vanillaWrapper("diamond_ores");
-    public static final TagKey<Block> LAPIS = vanillaWrapper("lapis_ores");
+    public static final TagKey<Block> DIAMOND = makeWrapperTag("ores/diamond");
+    public static final TagKey<Block> LAPIS = makeWrapperTag("ores/lapis");
     public static final TagKey<Block> ALUMINIUM = makeWrapperTag("ores/aluminum");
-    public static final TagKey<Block> COPPER = vanillaWrapper("copper_ores");
+    public static final TagKey<Block> COPPER = makeWrapperTag("ores/copper");
     public static final TagKey<Block> TIN = makeWrapperTag("ores/tin");
     public static final TagKey<Block> LEAD = makeWrapperTag("ores/lead");
     public static final TagKey<Block> SILVER = makeWrapperTag("ores/silver");
-    public static final TagKey<Block> GOLD = vanillaWrapper("gold_ores");
+    public static final TagKey<Block> GOLD = makeWrapperTag("ores/gold");
     public static final TagKey<Block> URANIUM = makeWrapperTag("ores/uranium");
     public static final TagKey<Block> NICKEL = makeWrapperTag("ores/nickel");
-    public static final TagKey<Block> IRON = vanillaWrapper("iron_ores");
+    public static final TagKey<Block> IRON = makeWrapperTag("ores/iron");
     public static final TagKey<Block> ZINC = makeWrapperTag("ores/zinc");
-    public static final TagKey<Block> EMERALD = vanillaWrapper("emerald_ores");
-    public static final TagKey<Block> COAL = vanillaWrapper("coal_ores");
-    public static final TagKey<Block> REDSTONE = vanillaWrapper("redstone_ores");
+    public static final TagKey<Block> EMERALD = makeWrapperTag("ores/emerald");
+    public static final TagKey<Block> COAL = makeWrapperTag("ores/coal");
+    public static final TagKey<Block> REDSTONE = makeWrapperTag("ores/redstone");
     public static final TagKey<Block> OSMIUM = makeWrapperTag("ores/osmium");
     public static final TagKey<Block> PLATINUM = makeWrapperTag("ores/platinum");
     public static final TagKey<Block> NETHERITE = makeWrapperTag("ores/netherite_scrap");
