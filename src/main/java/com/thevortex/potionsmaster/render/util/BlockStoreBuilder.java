f652d43990c17c36e42a97abd1ec0235f6176f27
thevortex <james.g.bowley@gmail.com>
2026-03-30 17:32:49 -0600

    Update BlockStoreBuilder.java
    fix default performance
    resolves another issue raised in #20

--- src/main/java/com/thevortex/potionsmaster/render/util/BlockStoreBuilder.java
+++ src/main/java/com/thevortex/potionsmaster/render/util/BlockStoreBuilder.java
@@ -85,16 +85,16 @@ public class BlockStoreBuilder {
         // If no config files exist, use default ore list so recipes can be generated
         if (list.isEmpty()) {
             PotionsMaster.LOGGER.info("No ore config files found, using default ore list");
-            list.add(new BlockData("coal", "#c:ores/coal", 0xFF202020, false, 0,"minecraft:coal"));
-            list.add(new BlockData("emerald", "#c:ores/emerald", 0xFF00FF00, false, 0,"minecraft:emerald"));
-            list.add(new BlockData("lapis", "#c:ores/lapis", 0xFF0000FF, false, 0,"minecraft:lapis_lazuli"));
-            list.add(new BlockData("redstone", "#c:ores/redstone", 0xFFFF0000, false, 0,"minecraft:redstone"));
-            list.add(new BlockData("diamond", "#c:ores/diamond", 0xFF3DDBE3, false, 0,"minecraft:diamond"));
-            list.add(new BlockData("iron", "#c:ores/iron", 0xFFE4C0AA, false, 0,"minecraft:raw_iron"));
-            list.add(new BlockData("gold", "#c:ores/gold", 0xFFD4AF37, false, 0,"minecraft:raw_gold"));
-            list.add(new BlockData("netherite", "#c:ores/ancient_debris", 0xFFFFA500, false, 0,"minecraft:netherite_scrap"));
-            list.add(new BlockData("quartz", "#c:ores/nether_quartz", 0xFFFFFFFF, false, 0,"minecraft:quartz"));
-            list.add(new BlockData("copper", "#c:ores/copper", 0xFFB7703A, false, 0,"minecraft:raw_copper"));
+            list.add(new BlockData("coal", "c:ores/coal", 0xFF202020, false, 0,"minecraft:coal"));
+            list.add(new BlockData("emerald", "c:ores/emerald", 0xFF00FF00, false, 0,"minecraft:emerald"));
+            list.add(new BlockData("lapis", "c:ores/lapis", 0xFF0000FF, false, 0,"minecraft:lapis_lazuli"));
+            list.add(new BlockData("redstone", "c:ores/redstone", 0xFFFF0000, false, 0,"minecraft:redstone"));
+            list.add(new BlockData("diamond", "c:ores/diamond", 0xFF3DDBE3, false, 0,"minecraft:diamond"));
+            list.add(new BlockData("iron", "c:ores/iron", 0xFFE4C0AA, false, 0,"minecraft:raw_iron"));
+            list.add(new BlockData("gold", "c:ores/gold", 0xFFD4AF37, false, 0,"minecraft:raw_gold"));
+            list.add(new BlockData("netherite", "c:ores/ancient_debris", 0xFFFFA500, false, 0,"minecraft:netherite_scrap"));
+            list.add(new BlockData("quartz", "c:ores/nether_quartz", 0xFFFFFFFF, false, 0,"minecraft:quartz"));
+            list.add(new BlockData("copper", "c:ores/copper", 0xFFB7703A, false, 0,"minecraft:raw_copper"));
         }
 
         PotionsMaster.blockStore.setStore(list);
